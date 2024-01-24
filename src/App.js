0d23225e71412b7b198f7eb72bf630e80a2a62a4
jlam303 <110576725+jlam303@users.noreply.github.com>
2024-01-24 14:15:35 -0700

    good

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useReducer } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [tasks, dispatch] = useReducer(reducer, {
+    taskN: 'Eat',
+    desc: 'eat the food',
+    category: 'food',
+    status: true,
+  });
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'doney':
+        return state.map((task) => {
+          if (task.id === action.id) {
+            return { ...task, status: true };
+          } else {
+            return task;
+          }
+        });
+      default:
+        return state;
+    }
+  };
+  return <div className='App'></div>;
 }
 
 export default App;
