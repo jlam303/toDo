5ed470ce74209d1d1f31b9350cbfedbc3888396d
jlam303 <110576725+jlam303@users.noreply.github.com>
2024-01-25 14:37:40 -0700

    tasks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useReducer } from 'react';
 function App() {
-  const [tasks, dispatch] = useReducer(reducer, {
-    taskN: 'Eat',
-    desc: 'eat the food',
-    category: 'food',
-    status: true,
-  });
   const reducer = (state, action) => {
     switch (action.type) {
-      case 'doney':
+      case 'addo':
         return state.map((task) => {
           if (task.id === action.id) {
             return { ...task, status: true };
+          } else {
+            return task;
+          }
+        });
+      case 'deleto':
+        return state.map((task) => {
+          if (task.id === action.id) {
+            return false;
           } else {
             return task;
           }
@@ -20,7 +22,38 @@ function App() {
         return state;
     }
   };
-  return <div className='App'></div>;
+  const [tasks, dispatch] = useReducer(reducer, [
+    {
+      id: 0,
+      name: 'Eat',
+      desc: 'eat the food',
+      category: 'food',
+      status: true,
+    },
+  ]);
+  const addTask = (id) => {
+    dispatch({ type: 'addo', id: id });
+  };
+  const deleteTask = (id) => {
+    dispatch({ type: 'deleto', id: id });
+  };
+  return (
+    <div className='App'>
+      {tasks.map((e, i) => {
+        if (e) {
+          return (
+            <div key={i}>
+              <h2>{e.category}</h2>
+              <h2>{e.name}</h2>
+              <h2>{e.desc}</h2>
+              <button onClick={() => deleteTask(e.id)}>Delete Task</button>;
+            </div>
+          );
+        } else return null;
+      })}
+      <button onClick={() => addTask()}>Add Task</button>
+    </div>
+  );
 }
 
 export default App;
