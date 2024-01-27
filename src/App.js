2df1cf6293374b74a45dcbe29516086099254b05
Sarc4sticNerd <jplam904@gmail.com>
2024-01-27 01:26:25 -0700

    e

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useReducer, useState } from 'react';
+import { useReducer, useState, useEffect } from 'react';
 function App() {
   const [name, setName] = useState('');
   const [cat, setCat] = useState('General');
@@ -11,16 +11,12 @@ function App() {
 
   const reducerTask = (state, action) => {
     switch (action.type) {
+      case 'set':
+        return [...action.local];
       case 'addo':
         return [...state, action.task];
       case 'deleto':
-        return state.map((task) => {
-          if (task.id === action.id) {
-            return false;
-          } else {
-            return task;
-          }
-        });
+        return state.filter((task) => task.id !== action.id);
       case 'edito':
         return state.map((task) => {
           if (task.id === action.id) {
@@ -35,16 +31,12 @@ function App() {
   };
   const reducerCat = (state, action) => {
     switch (action.type) {
+      case 'set':
+        return [...action.local];
       case 'addo':
         return [...state, action.cat];
       case 'deleto':
-        return state.map((task) => {
-          if (task === action.name) {
-            return false;
-          } else {
-            return task;
-          }
-        });
+        return state.filter((task) => task !== action.name);
       case 'edito':
         return state.map((task) => {
           if (task === action.id) {
@@ -57,15 +49,30 @@ function App() {
         return state;
     }
   };
-  const [tasks, dispatch] = useReducer(reducerTask, [
-    {
-      id: 0,
-      name: 'Eat',
-      desc: 'eat the food',
-      category: 'Food',
-    },
-  ]);
-  const [categories, dispatcho] = useReducer(reducerCat, ['General', 'Food']);
+  const [tasks, dispatch] = useReducer(
+    reducerTask,
+    localStorage.getItem('tasks')
+      ? JSON.parse(localStorage.getItem('tasks'))
+      : [
+          {
+            id: 0,
+            name: 'Eat',
+            desc: 'eat the food',
+            category: 'Food',
+          },
+        ]
+  );
+  const [categories, dispatcho] = useReducer(
+    reducerCat,
+    localStorage.getItem('cats')
+      ? JSON.parse(localStorage.getItem('cats'))
+      : ['General', 'Food']
+  );
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('cats', JSON.stringify(categories));
+  }, [tasks, categories]);
+
   const addTask = () => {
     if (update) {
       setUpdate(false);
@@ -82,17 +89,22 @@ function App() {
       setDesc('');
       setName('');
       setCat('General');
+      console.log(tasks);
+
       return;
     }
     dispatch({
       type: 'addo',
       task: {
-        id: tasks[tasks.length - 1] ? tasks[tasks.length - 1].id + 1 : 0,
+        id: tasks[tasks.length - 1]
+          ? Number(tasks[tasks.length - 1].id) + 1
+          : 0,
         name: name,
         desc: desc,
         category: cat,
       },
     });
+
     setDesc('');
     setName('');
     setCat('General');
@@ -105,13 +117,16 @@ function App() {
         id: upId2,
         cat: cato,
       });
+
       setCato('');
+
       return;
     }
     dispatcho({
       type: 'addo',
       cat: cato,
     });
+
     setCato('');
   };
   const deleteTask = (id) => {
@@ -121,7 +136,8 @@ function App() {
     setDesc(e.desc);
     setName(e.name);
     setId(e.id);
-    setCat(e.cat);
+    console.log(e.id);
+    setCat('General');
     setUpdate(true);
   };
   const deleteCat = (name) => {
@@ -133,83 +149,87 @@ function App() {
     setUpdate2(true);
   };
   return (
-    <div className='App'>
-      <form action=''>
-        <label htmlFor='name'>Name:</label>
-        <input
-          type='text'
-          name='name'
-          id='name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label htmlFor='desc'>Description:</label>
-        <input
-          type='text'
-          name='desc'
-          id='desc'
-          value={desc}
-          onChange={(e) => setDesc(e.target.value)}
-        />
-        <label htmlFor='cats'>Category:</label>
-        <select
-          onChange={(e) => {
-            setCat(e.target.value);
-          }}
-          value={cat}
-          name='cats'
-          id='cats'>
-          {categories.map((e, i) => {
-            if (e) {
-              return (
-                <option key={i} value={e}>
-                  {e}
-                </option>
-              );
-            } else return null;
-          })}
-        </select>
-        <button type='reset' onClick={() => addTask()}>
-          {update ? 'Update' : 'Add Task'}
-        </button>
-      </form>
-      {tasks.map((e, i) => {
-        if (e) {
-          return (
-            <div key={i}>
-              <h3>{e.category}</h3>
-              <h4>{e.name}</h4>
-              <p>{e.desc}</p>
-              <button onClick={() => deleteTask(e.id)}>Delete Task</button>
-              <button onClick={() => editTask({ ...e })}>Edit Task</button>
-            </div>
-          );
-        } else return null;
-      })}
-      <form action=''>
-        <label htmlFor='cat'>Category:</label>
-        <input
-          type='text'
-          name='cat'
-          id='cat'
-          value={cato}
-          onChange={(e) => setCato(e.target.value)}
-        />
-        <button type='reset' onClick={() => addCat()}>
-          {update2 ? 'Update' : 'Add Categoy'}
-        </button>
-      </form>
-      {categories.map((e, i) => {
-        if (e) {
-          return (
-            <div className='flexy' key={i}>
-              <h3>{e}</h3>
-              <button onClick={() => deleteCat(e)}>Delete Category</button>
-              <button onClick={() => editCat(e)}>Edit Category</button>
-            </div>
-          );
-        } else return null;
-      })}
+    <div className='App flexy'>
+      <div>
+        <form action=''>
+          <label htmlFor='name'>Name:</label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor='desc'>Description:</label>
+          <input
+            type='text'
+            name='desc'
+            id='desc'
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <label htmlFor='cats'>Category:</label>
+          <select
+            onChange={(e) => {
+              setCat(e.target.value);
+            }}
+            value={cat}
+            name='cats'
+            id='cats'>
+            {categories.map((e, i) => {
+              if (e) {
+                return (
+                  <option key={i} value={e}>
+                    {e}
+                  </option>
+                );
+              } else return null;
+            })}
+          </select>
+          <button type='reset' onClick={() => addTask()}>
+            {update ? 'Update' : 'Add Task'}
+          </button>
+        </form>
+        {tasks.map((e, i) => {
+          if (e) {
+            return (
+              <div key={i}>
+                <h3>{e.category}</h3>
+                <h4>{e.name}</h4>
+                <p>{e.desc}</p>
+                <button onClick={() => deleteTask(e.id)}>Delete Task</button>
+                <button onClick={() => editTask({ ...e })}>Edit Task</button>
+              </div>
+            );
+          } else return null;
+        })}
+      </div>
+      <div>
+        <form action=''>
+          <label htmlFor='cat'>Category:</label>
+          <input
+            type='text'
+            name='cat'
+            id='cat'
+            value={cato}
+            onChange={(e) => setCato(e.target.value)}
+          />
+          <button type='reset' onClick={() => addCat()}>
+            {update2 ? 'Update' : 'Add Category'}
+          </button>
+        </form>
+        {categories.map((e, i) => {
+          if (e) {
+            return (
+              <div className='flexy' key={i}>
+                <h3>{e}</h3>
+                <button onClick={() => deleteCat(e)}>Delete Category</button>
+                <button onClick={() => editCat(e)}>Edit Category</button>
+              </div>
+            );
+          } else return null;
+        })}
+      </div>
     </div>
   );
 }
